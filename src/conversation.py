cad3c3082beab01841eec438ff7124b0fa92cce5
Roland Szabo <rolisz@gmail.com>
2017-03-30 22:30:02 +0200

    Improve conversation splitting

--- src/conversation.py
+++ src/conversation.py
@@ -2,39 +2,47 @@ import logging
 import collections
 import datetime
 
-Message = collections.namedtuple('Message', ['contact', 'timestamp', 'message'])
+Message = collections.namedtuple('Message', ['datetime', 'contact', 'sender', 'message'])
 
 class Conversation(object):
 
-    def __init__(self, messages, me="Roland Szabo", other=None):
-        if type(messages[0]) == dict:
-            messages = [Message(**m) for m in messages]
-        contacts = set(m.contact for m in messages)
+    def __init__(self, messages, me="Roland Szabo"):
         self.me = me
-        if other is not None:
-            self.other = other
-        else:
-            try:
-                contacts.remove(me)
-                self.other = contacts.pop()
-            except KeyError:
-                logging.warning("Couldn't identify conversation participants: %s",
-                                contacts)
+        self.contacts = set(m.sender for m in messages)
+        self.participants = self.contacts.copy()
+        try:
+            self.contacts.remove(me)
+        except KeyError:
+            logging.info("Found discussion where you're not talking at all!")
         self.messages = messages
 
-    def _segment(self, break_length):
-        """Splits the conversation up when a break longer than x happened"""
+    def conversations(self, break_length=7200):
+        """Splits the conversation up when a break longer than break_length in seconds happened"""
+        conv = []
+        prev_timestamp = 0
+        curr_conv = []
+        for message in self.messages:
+            ts = time.mktime(time.strptime(message.datetime, '%Y-%m-%dT%H:%M:%S'))
+            if ts - prev_timestamp > break_length:
+                if len(curr_conv):  # Should be empty only for the first list
+                    conv.append(Conversation(curr_conv, self.me))
+                curr_conv = []
+            curr_conv.append(message)
+            prev_timestamp = ts
+        if len(curr_conv):
+            conv.append(Conversation(curr_conv, self.me))
+        return conv
 
     def char_length(self):
         lengths = collections.defaultdict(int)
         for reply in self.messages:
-            lengths[reply.contact] += len(reply.message)
+            lengths[reply.sender] += len(reply.message)
         return lengths
 
     def reply_length(self):
         lengths = collections.defaultdict(int)
         for reply in self.messages:
-            lengths[reply.contact] += 1
+            lengths[reply.sender] += 1
         return lengths
 
     def avg_reply_length(self):
@@ -42,19 +50,32 @@ class Conversation(object):
         counts = collections.defaultdict(int)
         avgs = {}
         for reply in self.messages:
-            counts[reply.contact] += 1
-            lengths[reply.contact] += len(reply.message)
+            counts[reply.sender] += 1
+            lengths[reply.sender] += len(reply.message)
         for name in lengths:
-            avgs[name] = lengths[name]*1.0/counts[name]
+            avgs[name] = lengths[name] / counts[name]
         return avgs
 
     def char_ratio(self):
+        """Return the ratio between 'me' and how much others wrote"""
         chars = self.char_length()
-        return chars[self.other]*1.0/chars[self.me]
+        return sum(chars[o] for o in self.contacts) / chars[self.me]
+
+    def char_percentage(self):
+        """Returns the percentage from the total how much each participant wrote."""
+        chars = self.char_length()
+        total = sum(chars.values())
+        return {c: chars[c] / total for c in self.participants}
 
     def reply_ratio(self):
         chars = self.reply_length()
-        return chars[self.other]*1.0/chars[self.me]
+        return sum(chars[o] for o in self.contacts) / chars[self.me]
+
+    def reply_percentage(self):
+        """Returns the percentage from the total how much each participant wrote."""
+        chars = self.reply_length()
+        total = sum(chars.values())
+        return {c: chars[c] / total for c in self.participants}
 
     def __len__(self):
         return sum(len(m.message) for m in self.messages)
@@ -63,11 +84,31 @@ class Conversation(object):
         """Returns the messages grouped by days, as Conversation objects"""
         days = collections.defaultdict(list)
         for message in self.messages:
-            day = message.timestamp.strftime("%Y-%m-%d")
+            day = message.datetime[:10]
             days[day].append(message)
         for day in days:
-            days[day] = Conversation(days[day], self.me, self.other)
+            days[day] = Conversation(days[day], self.me)
         return days
+
+    def __str__(self):
+        return "Participants: %s. Messages: %s" % (self.participants, len(self.messages))
+
+    __repr__ = __str__
+
+    def average_gap(self):
+        """Returns the average number of seconds between messages."""
+        gaps = self.gaps()
+        if len(gaps) == 0:
+            return 0
+        return sum(gaps)/len(gaps)
+
+    def gaps(self):
+        gaps = []
+        for i in range(1, len(self.messages)):
+            ts1 = time.mktime(time.strptime(self.messages[i-1].datetime, '%Y-%m-%dT%H:%M:%S'))
+            ts2 = time.mktime(time.strptime(self.messages[i].datetime, '%Y-%m-%dT%H:%M:%S'))
+            gaps.append(ts2-ts1)
+        return gaps
 
     def starters(self):
         pass
@@ -80,22 +121,3 @@ class Conversation(object):
 
     def dedupe(self):
         pass
-
-
-if __name__ == "__main__":
-    import parsers
-    import itertools
-    messages = collections.defaultdict(list)
-    for contact, text in parsers.Whatsapp("./Whatsapp"):
-        messages[contact].append(text)
-    for contact in messages:
-        messages[contact] = list(itertools.chain.from_iterable(messages[contact]))
-        messages[contact].sort(key=lambda x: x['timestamp'])
-    # for k in messages:
-    #     print k, len(messages[k])
-    # for k in messages:
-    #     print k, Conversation(messages[k]).reply_ratio()
-    # for k in messages:
-    #     print k, Conversation(messages[k]).char_ratio()
-    # for k in messages:
-    #     print k, len(Conversation(messages[k]).days().values()[-1])
